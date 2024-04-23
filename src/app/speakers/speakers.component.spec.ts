e6427a6360d9e90f8e6fd5f569d30d8178a80c44
Max Powell <maxfpowell@gmail.com>
2024-04-23 23:19:58 +0100

    feat(speakers): Add filtering to speaker list

--- src/app/speakers/speakers.component.spec.ts
+++ src/app/speakers/speakers.component.spec.ts
@@ -1,23 +1,78 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import { SpeakersComponent } from './speakers.component';
+import { SpeakersService } from './services';
+import { signal } from '@angular/core';
+import { Speaker } from '../shared/interfaces';
+
+const mockSpeakers: Speaker[] = [
+  {
+    name: {
+      title: 'Mr',
+      first: 'Klaus-Jürgen',
+      last: 'Asmus',
+    },
+    location: {
+      city: 'Trostberg',
+      country: 'Germany',
+    },
+    email: 'klaus-jurgen.asmus@example.com',
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/3.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/3.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/3.jpg',
+    },
+    nat: 'DE',
+  },
+  {
+    name: {
+      title: 'Miss',
+      first: 'Ömür',
+      last: 'Eronat',
+    },
+    location: {
+      city: 'Bitlis',
+      country: 'Turkey',
+    },
+    email: 'omur.eronat@example.com',
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/36.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/36.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/36.jpg',
+    },
+    nat: 'TR',
+  },
+];
 
 describe('SpeakersComponent', () => {
   let component: SpeakersComponent;
   let fixture: ComponentFixture<SpeakersComponent>;
 
+  const mockSpeakersService = jasmine.createSpyObj<SpeakersService>(
+    'SpeakersService',
+    { getSpeakers: signal(mockSpeakers) },
+  );
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SpeakersComponent]
-    })
-    .compileComponents();
-    
+      imports: [SpeakersComponent],
+      providers: [{ provide: SpeakersService, useValue: mockSpeakersService }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SpeakersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should filter the list of users when the value is entered', fakeAsync(() => {
+    expect(component.filteredSpeakers()).toEqual(mockSpeakers);
+    component.filter.patchValue('ero');
+    tick(500);
+    expect(component.filteredSpeakers().length).toBe(1);
+    expect(component.filteredSpeakers()[0]).toBe(mockSpeakers[1]);
+  }));
 });
--- src/app/speakers/speakers.component.ts
+++ src/app/speakers/speakers.component.ts
@@ -1,15 +1,43 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
 import { SpeakersService } from './services';
 import { SpeakerCardComponent } from './components';
+import {
+  FormControl,
+  FormControlDirective,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { debounce, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-speakers',
   standalone: true,
-  imports: [SpeakerCardComponent],
+  imports: [SpeakerCardComponent, ReactiveFormsModule],
   templateUrl: './speakers.component.html',
   styleUrl: './speakers.component.scss',
 })
 export class SpeakersComponent {
   private speakersService = inject(SpeakersService);
-  speakers = this.speakersService.getSpeakers();
+
+  filter = new FormControl<string | null>(null);
+
+  private speakers = this.speakersService.getSpeakers();
+  private filterString = toSignal(
+    this.filter.valueChanges.pipe(debounceTime(500)),
+  );
+  filteredSpeakers = computed(() => {
+    const speakers = this.speakers();
+    const filterString = this.filterString()?.toLowerCase();
+    if (!speakers) {
+      return [];
+    }
+    if (!filterString) {
+      return speakers;
+    }
+    return speakers.filter(
+      (speaker) =>
+        speaker.name.first.toLowerCase().includes(filterString) ||
+        speaker.name.last.toLowerCase().includes(filterString),
+    );
+  });
 }
